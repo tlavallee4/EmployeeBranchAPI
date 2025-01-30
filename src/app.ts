ab63c43c8d24d9185a2fad27de00f74ad6cc954b
tanellelavallee <tanellelavallee@gmail.com>
2025-01-30 11:38:03 -0600

    Testing: Testing if the server runs as expected

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 
+// // import itemRoutes
+// import itemRoutes from "./api/v1/routes/itemRoutes";
+
 const app = express();
 
 // Use Morgan for HTTP request logging
 app.use(morgan("combined"));
+
+app.use(express.json());
 
 app.get("/health", (req, res) => {
 	res.send("Server is healthy");
@@ -14,5 +19,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
+
+// // register itemRoutes
+// app.use("/api/v1/items", itemRoutes)
 
 export default app;
