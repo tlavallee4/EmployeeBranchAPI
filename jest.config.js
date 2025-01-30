69c1218046dec690a85e5247c0d482f419700e04
tanellelavallee <tanellelavallee@gmail.com>
2025-01-30 11:42:41 -0600

    Jest: INstall Jest ts and added jest packages to script

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node"
+};
+
+// 'preset': "ts-jest": use ts-jest present to handle TS files
+// testEnvironment: "node" - setting env to node
+
