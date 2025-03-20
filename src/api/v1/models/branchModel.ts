14e2e097d6c1c42048a87d78d4f191360cc3c2b4
tanellelavallee <tanellelavallee@gmail.com>
2025-03-20 16:28:21 -0500

    feat: Added detailed documentation for branch model

--- src/api/v1/models/branchModel.ts
+++ src/api/v1/models/branchModel.ts
@@ -1,8 +1,27 @@
-import { CreateReadStreamOptions } from "fs/promises";
-
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Branch:
+ *       type: object
+ *       properties:
+ *         branchId:
+ *           type: string
+ *           description: The unique identifier for a branch.
+ *         branchName:
+ *           type: string
+ *           description: The name of the branch.
+ *         branchAddress:
+ *           type: string
+ *           description: The physical address of the branch.
+ *         branchPhone:
+ *           type: string
+ *           description: The contact phone number for the branch.
+ */
 export interface Branch {
     branchId: string;
-    branchName: string; 
+    branchName: string;
     branchAddress: string;
     branchPhone: string;
-}
+  }
+  
